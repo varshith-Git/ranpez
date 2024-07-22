93254ebc15e1b8ab60431e3f4412012575a3032f
varshith G <varshith.gudur17@gmail.com>
2024-07-23 00:42:13 +0530

    libraries added

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ranpez',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pyarrow',
+        'openpyxl'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ranpez=main:main',
+        ],
+    },
+)
